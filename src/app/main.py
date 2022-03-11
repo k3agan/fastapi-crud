5e5e8c90d0a2f48bba1485fd01dcfb9056053f8f
kodonoghue@factorsgroup.com <k3agan@gmail.com>
2022-03-11 01:12:25 -0800

    test

--- src/app/main.py
+++ src/app/main.py
@@ -6,3 +6,17 @@ app = FastAPI()
 @app.get("/ping")
 def pong():
     return {"ping": "pong!"}
+
+
+@app.get("/sequence")
+def pong():
+    return {"sequence": ["pong!0", "pong!1", "pong!2"]}
+
+# Minimum viable (with just the plotly data stuff) graph for testing
+
+
+# @app.get("/graph")
+# def graph():
+#     plt.plot([1, 2, 3, 4])
+
+#     return
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,3 +1,4 @@
+from numpy import NaN
 from starlette.testclient import TestClient
 
 from app.main import app
@@ -9,3 +10,27 @@ def test_ping():
     response = client.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong!"}
+
+
+def test_ping_value():
+    response = client.get("/ping")
+    assert response.json()['ping'] == 'pong!'
+
+
+def test_sequence():
+    response = client.get("/sequence")
+    assert response.status_code == 200
+    assert response.json()['sequence'][1] == 'pong!1'
+    assert len(response.json()['sequence']) >= 3
+
+
+def test_length():
+    response = client.get("/sequence")
+    assert response.status_code == 200
+    assert len(response.json()['sequence']) >= 3
+
+
+def test_length_not_null():
+    response = client.get("/sequence")
+    assert response.status_code == 200
+    assert len(response.json()['sequence']) != NaN
