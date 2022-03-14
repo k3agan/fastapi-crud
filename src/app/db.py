f6fdc63042f0c1db876dd790d89b68080e270680
kodonoghue@factorsgroup.com <k3agan@gmail.com>
2022-03-14 14:57:34 -0700

    wired up the database, startup & shutdown handlers

--- src/app/db.py
+++ src/app/db.py
@@ -1,12 +1,32 @@
 import os
+
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    DateTime,
+    Table,
+    MetaData
+)
+
+from sqlalchemy.sql import func
+
 from databases import Database
-from sqlalchemy import create_engine, MetaData
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 # SQLAlchemy
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
+notes = Table(
+    "notes",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(50)),
+    Column("descirption", String(50)),
+    Column("created_date", DateTime, default = func.now(), nullable = False)
+    )
 
 # databases Query builder
 database = Database(DATABASE_URL)
--- src/app/main.py
+++ src/app/main.py
@@ -1,15 +1,27 @@
+from fastapi import FastAPI
+from app.db import engine, database, metadata
+
 from app.api import ping
 from app.api import sequence
-from fastapi import FastAPI
 
 app = FastAPI()
+
+metadata.create_all(engine)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 app.include_router(ping.router)
 app.include_router(sequence.router)
 
-
 # Minimum viable (with just the plotly data stuff) graph for testing
-
 
 # @app.get("/graph")
 # def graph():
